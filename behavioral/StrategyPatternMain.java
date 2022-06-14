5f0f28bcf74ae70c9697f3a337e91288bec92010
daelim.im <blackway76@gmail.com>
2022-06-14 17:50:34 +0900

    StrategyPatttern 스트레이테지 패턴 추가

---
+++ behavioral/StrategyPatternMain.java
@@ -0,0 +1,112 @@
+package behavioral;
+
+import java.util.ArrayList;
+import java.util.List;
+
+interface PaymentStrategy {
+    public void pay(int amount);
+}
+
+class CreditCardStrategy implements PaymentStrategy {
+
+    private String name;
+    private String cardNumber;
+    private String cvv;
+    private String dateOfExpiry;
+
+    public CreditCardStrategy(String name, String cardNumber, String cvv, String dateOfExpiry) {
+        this.name = name;
+        this.cardNumber = cardNumber;
+        this.cvv = cvv;
+        this.dateOfExpiry = dateOfExpiry;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println(amount + " paid with credit/debit Card");
+    }
+}
+
+class PaypalStrategy implements PaymentStrategy {
+
+    private String emailId;
+    private String password;
+
+    public PaypalStrategy(String emailId, String password) {
+        this.emailId = emailId;
+        this.password = password;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println(amount + " paid using Paypal");
+    }
+}
+
+class Item {
+    private String upcCode;
+    private int price;
+
+    public Item(String upcCode, int price) {
+        this.upcCode = upcCode;
+        this.price = price;
+    }
+
+    public String getUpcCode() {
+        return upcCode;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
+
+class ShoppingCart {
+
+	//List of items
+	List<Item> items;
+	
+	public ShoppingCart(){
+		this.items=new ArrayList<Item>();
+	}
+	
+	public void addItem(Item item){
+		this.items.add(item);
+	}
+	
+	public void removeItem(Item item){
+		this.items.remove(item);
+	}
+	
+	public int calculateTotal(){
+		int sum = 0;
+		for(Item item : items){
+			sum += item.getPrice();
+		}
+		return sum;
+	}
+	
+	public void pay(PaymentStrategy paymentMethod){
+		int amount = calculateTotal();
+		paymentMethod.pay(amount);
+	}
+}
+
+public class StrategePatternMain {
+
+    public static void main(String...args) {
+        ShoppingCart cart = new ShoppingCart();
+        Item item1 = new Item("1234", 10);
+        Item item2 = new Item("5678", 40);
+
+        cart.addItem(item1);
+        cart.addItem(item2);
+
+        //pay by Paypal
+        cart.pay(new PaypalStrategy("myemail@example.com", "mypwd"));
+
+        //pay by credit card
+        cart.pay(new CreditCardStrategy("Pankaj Kumar", "1234567890", "786", "12/15"));
+    }
+    
+}
