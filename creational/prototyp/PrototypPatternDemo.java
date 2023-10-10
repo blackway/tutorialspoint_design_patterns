378b94b5d882829c19f811122ceaaa8280f85ab6
Im Dae Lim <blackway76@gmail.com>
2023-10-10 21:41:51 +0900

    20231010 추가

--- creational/prototyp/PrototypPatternDemo.java
+++ creational/prototyp/PrototypPatternDemo.java
@@ -1,16 +1,65 @@
 package creational.prototyp;
 
+import java.util.Hashtable;
+
 public class PrototypPatternDemo {
     public static void main(String... args) {
         ShapeCache.loadCache();
 
         Shape clonedShape = (Shape) ShapeCache.getShape("1");
         System.out.println("Shape: " + clonedShape.getType());
+        Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+        System.out.println("Shape: " + clonedShape2.getType());
+        Shape clonedShape3 = (Shape) ShapeCache.getShape("3");
+        System.out.println("Shape: " + clonedShape3.getType());
+
+        Shape r1 =  (Shape) ShapeCache.getShape("3");
+        r1.setStrNm("a");
+        System.out.println("r1.setStrNm: " + r1.getStrNm());
+
+        Shape r2 =  (Shape) ShapeCache.getShape("3");
+        System.out.println("r2.setStrNm: " + r2.getStrNm());
+
+    }
+
+    class ShapeCache {
+        private static Hashtable<String, Shape> shapeMap = new Hashtable<String, Shape>();
+
+        public static Shape getShape(String shapeId) {
+            Shape cschedShape = shapeMap.get(shapeId);
+            return (Shape) cschedShape.clone();
+        }
+
+        //for each shape run database query and create shape
+        //shapeMap.put(shapeKey, shape);
+        //for example, we are adding three shapes
+
+        public static void loadCache() {
+            Circle circle = new PrototypPatternDemo().new Circle();
+            circle.setId("1");
+            shapeMap.put(circle.getId(), circle);
+            Square square = new PrototypPatternDemo().new Square();
+            square.setId("2");
+            shapeMap.put(square.getId(), square);
+            Rectangle rectangle = new PrototypPatternDemo().new Rectangle();
+            rectangle.setId("3");
+            shapeMap.put(rectangle.getId(), rectangle);
+        }
+
     }
 
     abstract class Shape implements Cloneable {
         private String id;
         protected String type;
+        private String strNm;
+
+        public String getStrNm() {
+            return strNm;
+        }
+
+        public void setStrNm(String strNm) {
+            this.strNm = strNm;
+        }
 
         abstract void draw();
 
@@ -40,7 +89,7 @@ public class PrototypPatternDemo {
         }
     }
 
-    public class Rectangle extends Shape {
+    class Rectangle extends Shape {
         public Rectangle() {
             type = "Rectangle";
         }
@@ -50,7 +99,7 @@ public class PrototypPatternDemo {
             System.out.println("Inside Rectangle::draw() method.");
         }
     }
-    public class Square extends Shape {
+    class Square extends Shape {
         public Square() {
             type = "Square";
         }
@@ -60,9 +109,9 @@ public class PrototypPatternDemo {
             System.out.println("Inside Square::draw() method.");
         }
     }
-    public class Circle extends Shape {
+    class Circle extends Shape {
         public Circle() {
-            type = "Square";
+            type = "Circle";
         }
 
         @Override
---
+++ creational/prototyp/PrototypeFactory.java
@@ -0,0 +1,28 @@
+package creational.prototyp;
+
+import java.util.Map;
+
+public class PrototypeFactory {
+    public static void main(String...args) {
+        // if(args.length > 0) {
+        //     for(String type : args) {
+        //         Person prototype = Factory.getPrototype(type);
+        //         if(prototype != null) {
+        //             System.out.println(prototype);
+        //         }
+        //     }
+        // } else {
+        //     System.out.println("Run again with arguments of command string");
+        // }
+    }
+
+    // class Factory {
+    //     private static final Map<String, Person> prototypes = new HashMap<>();
+
+    //     static {
+    //         prototypes.put("tom", new Tom());
+    //     }
+
+    //     public static 
+    // }
+}
